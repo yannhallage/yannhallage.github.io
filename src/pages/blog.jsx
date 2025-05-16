2514993227032879aa6026dde04db66b2ba249ad
yannhallage <contact.devhllg@gmail.com>
2025-05-16 22:54:02 +0000

    mise ajour sur l'interface

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -1,10 +1,32 @@
+import { motion } from "motion/react";
 
+import { Context } from "../context/context";
+import { useContext } from "react";
+import Alternative from "../alternative";
 
 const Blog = () => {
+    const { state, setState } = useContext(Context);
     return (
-        <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, numquam quae libero voluptatum est mollitia vero? Ducimus, in et autem ipsum repellendus, esse optio deleniti harum consequatur quisquam id pariatur?
-        </span>
+        <>
+            <section>
+                <span className=" text-[13px]md:text-2xl text-base text-neutral-300">Experience</span>
+                <span className=" text-[13px]md:text-2xl text-base float-end cursor-pointer text-neutral-300"
+                    onClick={() => {
+                        setState(
+                            <Alternative />
+                        );
+                    }}
+                ><i class="fa-solid fa-arrow-left"></i></span>
+                <hr className="text-secondary/5 dark:text-white/5 mb-4" />
+                <motion.div
+                    initial={{ opacity: 0, y: 30 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1, ease: "easeOut" }}
+                >
+
+                </motion.div>
+            </section>
+        </>
     )
 }
 export default Blog;
